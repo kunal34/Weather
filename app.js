6dbfc84ac99df588a72497e5d8cfc6a216e25b1d
kunal34 <55825231+kunal34@users.noreply.github.com>
2021-04-03 10:54:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ function showError(error){
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude){
-    let api = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-    
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    //https://cors-anywhere.herokuapp.com/
     fetch(api)
         .then(function(response){
             let data = response.json();
